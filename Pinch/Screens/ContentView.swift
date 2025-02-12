ce833a71d93e848a0f5c1aeb9cb1e5846dad003c
Kyaw-Zin-George <ktz1695@gmail.com>
2025-02-12 12:34:09 +0000

    Adding Linear Animation to the image

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: Property
+    @State private var isAnimating : Bool = false
+    
     var body: some View {
         NavigationView{
             ZStack{
@@ -18,9 +21,16 @@ struct ContentView: View {
                     .clipShape(RoundedRectangle(cornerRadius: 10))
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1:0)
+                    
             }.navigationTitle("Pinch and Zoom")
             .navigationBarTitleDisplayMode(.inline)
-        }
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            })
+            }
     }
 }
 
