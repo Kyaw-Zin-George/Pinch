a54e7d50d815cc9db980d5fabc17c0c7b1deceec
Kyaw-Zin-George <ktz1695@gmail.com>
2025-02-12 12:25:00 +0000

    Adding a front mag cover image into the app

---
+++ Pinch/Model/Variables.swift
@@ -0,0 +1,8 @@
+//
+//  Variables.swift
+//  Pinch
+//
+//  Created by Kyaw Thant Zin(George) on 2/12/25.
+//
+
+import Foundation
--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            ZStack{
+                //MARK: Page
+                Image(frontMagCover)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+            }.navigationTitle("Pinch and Zoom")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
