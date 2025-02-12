38bf062c86bb1229370225d2f81135463768f9f1
Kyaw-Zin-George <ktz1695@gmail.com>
2025-02-12 11:54:05 +0000

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Kyaw Thant Zin(George) on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
