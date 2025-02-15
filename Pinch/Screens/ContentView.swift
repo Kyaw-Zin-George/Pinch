4e1f491ca13997bf15714e9629735ae9a64e66e5
Kyaw-Zin-George <ktz1695@gmail.com>
2025-02-15 22:25:55 +0000

    creating a resetImageScale function to avoid code repetition

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -12,6 +12,13 @@ struct ContentView: View {
     @State private var isAnimating : Bool = false
     @State private var imageScale : CGFloat = 1
     @State private var imageOffset : CGSize = .zero
+    //MARK: Function
+    func resetImageState(){
+        return withAnimation(.spring()){
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     var body: some View {
         NavigationView{
@@ -33,9 +40,7 @@ struct ContentView: View {
                                 imageScale = 5
                             }
                         }else {
-                            withAnimation(.spring()){
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     }
                 //MARK: DRAG GESTURE
@@ -48,10 +53,7 @@ struct ContentView: View {
                         }
                             .onEnded({ _ in
                                 if imageScale <= 1{
-                                    withAnimation(.spring()) {
-                                        imageScale = 1
-                                        imageOffset = .zero
-                                    }
+                                    resetImageState()
                                 }
                             })
                     )
