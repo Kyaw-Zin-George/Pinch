7f2c66914ea608d5f89cc283f5ecdd826b96fe30
Kyaw-Zin-George <ktz1695@gmail.com>
2025-02-16 16:23:04 +0000

    Adding measurements with values into the infopanel

--- Pinch/Views/InfoPanelView.swift
+++ Pinch/Views/InfoPanelView.swift
@@ -8,11 +8,40 @@
 import SwiftUI
 
 struct InfoPanelView: View {
+    //MARK: PROPERTIES
+    var scale: CGFloat
+    var offset: CGSize
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            //MARK: HOTSPOT
+            Image(systemName: doubleCircle)
+                .symbolRenderingMode(.hierarchical)
+                .resizable()
+                .frame(width: 30, height: 30)
+            Spacer()
+            //MARK: INFO PANEL
+            HStack(spacing:2){
+                Image(systemName: arrowsuLdR)
+                Text("\(scale)")
+                
+                Spacer()
+                
+                Image(systemName: arrowsLR)
+                Text("\(offset.width)")
+                Spacer()
+                
+                Image(systemName: arrowsUD)
+                Text("\(offset.height)")
+                Spacer()
+            }
+        }
     }
 }
 
 #Preview {
-    InfoPanelView()
+    InfoPanelView(scale: 1, offset: .zero)
+        .preferredColorScheme(.dark)
+        .previewLayout(.sizeThatFits)
+        .padding()
 }
