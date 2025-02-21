8e975cfca72d8bf98faa546cf55672fbd1e041dd
Kyaw-Zin-George <ktz1695@gmail.com>
2025-02-21 21:37:17 +0000

    Building a custom structure for Page and local image data

---
+++ Pinch/Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by Kyaw Thant Zin(George) on 2/21/25.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+    Page(id: 1, imageName: frontMagCover),
+    Page(id: 2, imageName: backMagCover)
+]
---
+++ Pinch/Data/PageModel.swift
@@ -0,0 +1,13 @@
+//
+//  PageModel.swift
+//  Pinch
+//
+//  Created by Kyaw Thant Zin(George) on 2/21/25.
+//
+
+import Foundation
+
+struct Page: Identifiable {
+    let id: Int
+    let imageName: String
+}
